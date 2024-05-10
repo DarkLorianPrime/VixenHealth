8aef34abb836d7b73ed8134b40a0dd992c9a2127
darklorian <lolpokens@mail.ru>
2024-05-10 18:30:28 +0400

    update settings, (add psql url generator, add debug and level log)

---
+++ containers/vixenhealth_id/app/settings.py
@@ -0,0 +1,44 @@
+import logging
+from functools import cached_property
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
+from sqlalchemy import URL
+
+
+class Settings(BaseSettings):
+    DRIVER: str = "postgresql+"
+    POSTGRES_HOST: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_NAME: str
+
+    MINIO_HOST: str = Field(alias="MINIO_ROOT_HOST")
+    MINIO_USER: str = Field(alias="MINIO_ROOT_USER")
+    MINIO_PASSWORD: str = Field(alias="MINIO_ROOT_PASSWORD")
+
+    RABBIT_HOST: str = Field(alias="RABBITMQ_DEFAULT_HOST")
+    RABBIT_USER: str = Field(alias="RABBITMQ_DEFAULT_USER")
+    RABBIT_PASSWORD: str = Field(alias="RABBITMQ_DEFAULT_PASSWORD")
+
+    PROJECT_SECRET_KEY: str
+
+    DEBUG: bool = True
+    LOG_LEVEL: int = logging.INFO
+
+    @cached_property
+    def get_postgres_url(self) -> URL:
+        database_url = URL(
+            drivername=self.DRIVER + "asyncpg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            database=self.POSTGRES_NAME,
+            query={}  # type: ignore
+        )
+        return database_url
+
+
+settings = Settings()  # type: ignore
