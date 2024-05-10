6e1315d9c6f8b3d08659ae6de96edae73ef88335
darklorian <lolpokens@mail.ru>
2024-05-10 18:36:46 +0400

    create get_session function

--- containers/vixenhealth_id/app/storages/database/database.py
+++ containers/vixenhealth_id/app/storages/database/database.py
@@ -6,3 +6,8 @@ from containers.vixenhealth_id.app.settings import settings
 
 engine: AsyncEngine = create_async_engine(settings.database_url)
 sessionmaker = async_sessionmaker(engine, class_=AsyncEngine, expire_on_commit=False)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with sessionmaker() as sess:
+        yield sess
