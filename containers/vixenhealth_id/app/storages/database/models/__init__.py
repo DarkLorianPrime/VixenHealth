37e7855a3fd60130da4e3dfa0a698588fa3215b3
darklorian <lolpokens@mail.ru>
2024-05-13 21:16:17 +0400

    split models to directories

--- containers/vixenhealth_id/app/storages/database/models/__init__.py
+++ containers/vixenhealth_id/app/storages/database/models/__init__.py
@@ -1,7 +1,10 @@
-from storages.database.models.account import Account
+from storages.database.models.passport.passport import Passport
+from storages.database.models.account.account import Account
+
 from storages.database.models.__meta__ import Base
 
 __all__ = [
     'Base',
-    'Account'
+    'Account',
+    'Passport'
 ]
--- containers/vixenhealth_id/app/storages/database/models/passport/passport.py
+++ containers/vixenhealth_id/app/storages/database/models/passport/passport.py
@@ -1,11 +1,9 @@
 import datetime
-import uuid
 
-from sqlalchemy import String, Boolean, Integer, Date, UUID, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, Relationship
+from sqlalchemy import String, Boolean, Integer, Date
+from sqlalchemy.orm import Mapped, mapped_column
 
 from storages.database.models.__meta__ import Base, SoftDeleteMixin, CreatedUpdatedMixin
-from storages.database.types.file import File
 
 
 class Passport(Base, SoftDeleteMixin, CreatedUpdatedMixin):
@@ -22,19 +20,3 @@ class Passport(Base, SoftDeleteMixin, CreatedUpdatedMixin):
     issuing_place: Mapped[str] = mapped_column(String)
     birth_date: Mapped[datetime.date] = mapped_column(Date)
     birth_place: Mapped[str] = mapped_column(String)
-
-
-class Account(Base, SoftDeleteMixin, CreatedUpdatedMixin):
-    __tablename__ = "account"
-
-    username: Mapped[str] = mapped_column(String, nullable=False)
-    email: Mapped[str] = mapped_column(String, nullable=False)
-    password: Mapped[str] = mapped_column(String, nullable=False)
-    avatar: Mapped[str] = mapped_column(File(bucket_name="avatars"), nullable=False, default="default.png")
-    passport_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey("passport.id"),
-        default=None,
-        nullable=True
-    )
-    passport: Mapped["Passport"] = Relationship()
