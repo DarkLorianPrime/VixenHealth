8edc8ebe055f9eaf8697a23700050d1668ae030f
darklorian <lolpokens@mail.ru>
2024-05-13 16:02:58 +0400

    add model to alembic visible part

--- containers/vixenhealth_id/app/storages/database/models/__init__.py
+++ containers/vixenhealth_id/app/storages/database/models/__init__.py
@@ -1,7 +1,7 @@
-# from storages.database.models.account import Account
+from storages.database.models.account import Account
 from storages.database.models.__meta__ import Base
 
 __all__ = [
     'Base',
-    # 'Account'
+    'Account'
 ]
