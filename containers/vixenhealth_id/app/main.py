0aa998e350f10dbc75ffc9363e77fe93defe27cc
darklorian <lolpokens@mail.ru>
2024-05-13 08:41:23 +0400

    connect routers to application

--- containers/vixenhealth_id/app/main.py
+++ containers/vixenhealth_id/app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, APIRouter
 
+from containers.vixenhealth_id.app.routers import authentication_router, settings_router, profile_router
+
 
 def connect_routers(application: FastAPI) -> None:
     main_api_router = APIRouter(prefix="/api/v1")
+    main_api_router.include_router(authentication_router)
+    main_api_router.include_router(settings_router)
+    main_api_router.include_router(profile_router)
     application.include_router(main_api_router)
 
 
@@ -10,7 +15,7 @@ def create_app() -> FastAPI:
     application = FastAPI(
         title="VixenHealth ID",
         description="Сервис, отвечающий за аутентификацию и взаимодействие с профилем пользователей.",
-        version="0.0.1"
+        version="0.0.3"
     )
     return application
 
