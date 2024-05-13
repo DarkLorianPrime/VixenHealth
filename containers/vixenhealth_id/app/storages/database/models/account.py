dcfa61fa2aeb57122bb97cdb0404b0b554bfa013
darklorian <lolpokens@mail.ru>
2024-05-13 16:37:04 +0400

    add avatar field

--- containers/vixenhealth_id/app/storages/database/models/account.py
+++ containers/vixenhealth_id/app/storages/database/models/account.py
@@ -2,6 +2,7 @@ from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from storages.database.models.__meta__ import Base, SoftDeleteMixin, CreatedUpdatedMixin
+from storages.database.types.file import File
 
 
 class Account(Base, SoftDeleteMixin, CreatedUpdatedMixin):
@@ -10,3 +11,4 @@ class Account(Base, SoftDeleteMixin, CreatedUpdatedMixin):
     username: Mapped[str] = mapped_column(String, nullable=False)
     email: Mapped[str] = mapped_column(String, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
+    avatar: Mapped[str] = mapped_column(File, nullable=False, default="avatars/default.png")
