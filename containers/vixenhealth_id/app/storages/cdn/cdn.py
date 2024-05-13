6313806e9c993df6b503a0426b967275349aea4f
darklorian <lolpokens@mail.ru>
2024-05-13 16:41:44 +0400

    create init function for minio

--- containers/vixenhealth_id/app/storages/cdn/cdn.py
+++ containers/vixenhealth_id/app/storages/cdn/cdn.py
@@ -2,6 +2,8 @@ from minio import Minio
 
 from config.settings import settings
 
+AVATAR_BUCKET_NAME = "avatars"
+
 
 def get_minio():
     return Minio(
@@ -9,3 +11,18 @@ def get_minio():
         access_key=settings.MINIO_USER,
         secret_key=settings.MINIO_PASSWORD
     )
+
+
+def init_minio():
+    minio = get_minio()
+    if not minio.bucket_exists(AVATAR_BUCKET_NAME):
+        minio.make_bucket(AVATAR_BUCKET_NAME)
+
+        with open("assets/default.png", "rb") as default_avatar:
+            minio.put_object(
+                bucket_name=AVATAR_BUCKET_NAME,
+                object_name="default.png",
+                data=default_avatar,
+                length=-1,
+                part_size=10485760
+            )
