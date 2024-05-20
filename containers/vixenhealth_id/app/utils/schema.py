6ffb6d7d292ad761f501d15a670e0f680a29f226
darklorian <lolpokens@mail.ru>
2024-05-20 12:55:52 +0400

    Create as_form to auth

---
+++ containers/vixenhealth_id/app/utils/schema.py
@@ -0,0 +1,21 @@
+from json import JSONDecodeError
+from typing import Any, Dict
+
+from pydantic import BaseModel
+from starlette.datastructures import FormData
+from starlette.requests import Request
+
+
+class CustomModel(BaseModel):
+    @classmethod
+    async def as_form(cls, request: Request) -> 'CustomModel':
+        data: Dict[str, Any] | FormData
+        data = await request.form()
+
+        if not data:
+            try:
+                data = await request.json()
+            except JSONDecodeError:
+                data = {}
+
+        return cls(**data)
