d711d51056526b86f63cc3c6191faf2277eb3892
darklorian <lolpokens@mail.ru>
2024-05-28 15:26:51 +0400

    [update] add base repository

--- containers/vixenhealth_id/app/storages/database/models/__meta__.py
+++ containers/vixenhealth_id/app/storages/database/models/__meta__.py
@@ -1,8 +1,17 @@
+import abc
 import datetime
 import uuid
+from abc import abstractmethod
+from typing import Annotated
 
+from fastapi import Depends
+from minio import Minio
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import declarative_mixin, mapped_column, Mapped, DeclarativeBase
 from sqlalchemy import DateTime, func, UUID, Boolean
+
+from storages.cdn.cdn import get_minio
+from storages.database.database import get_session
 
 
 @declarative_mixin
@@ -27,3 +36,27 @@ class Base(DeclarativeBase):
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), unique=True, default=uuid.uuid4, primary_key=True
     )
+
+
+class BaseRepository(abc.ABC):
+    def __init__(
+        self,
+        session: Annotated[AsyncSession, Depends(get_session)],
+        minio: Annotated[Minio, Depends(get_minio)],
+    ):
+        self.session = session
+        self.minio = minio
+
+    @abstractmethod
+    async def is_exists(self, *args, **kwargs):
+        """exists"""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def find(self, *args, **kwargs):
+        """limit 1"""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def create(self, *args, **kwargs):
+        raise NotImplementedError
