ed20094eaa7e45869418bcde2849c7f736f95e29
darklorian <lolpokens@mail.ru>
2024-05-28 15:27:33 +0400

    [update] create role model

---
+++ containers/vixenhealth_id/app/storages/database/models/role.py
@@ -0,0 +1,34 @@
+from typing import Set, List
+
+from sqlalchemy import String, Table, Column, ForeignKey, Index
+from sqlalchemy.orm import Mapped, mapped_column, Relationship
+
+from storages.database.models import Base
+from storages.database.models.permission import Permission
+
+role_permissions = Table(
+    "role_permissions",
+    Base.metadata,
+    Column("role_id", ForeignKey("role.id")),
+    Column("permission_id", ForeignKey("permission.id")),
+    Column("application_id", ForeignKey("application.id")),
+)
+
+account_roles = Table(
+    "account_roles",
+    Base.metadata,
+    Column("role_id", ForeignKey("role.id")),
+    Column("account_id", ForeignKey("account.id")),
+    Column("application_id", ForeignKey("application.id")),
+)
+
+
+class Role(Base):
+    __tablename__ = "role"
+
+    name: Mapped[str] = mapped_column(String)
+    permissions: Mapped[List["Permission"]] = Relationship(
+        secondary=role_permissions, lazy="selectin"
+    )
+
+    __table_args__ = (Index("role_name", "name", unique=True),)
