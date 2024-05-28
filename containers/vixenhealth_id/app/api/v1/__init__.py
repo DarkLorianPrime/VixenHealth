d87803b176fcf4e48157c84e5e6f3a8cac759a5e
darklorian <lolpokens@mail.ru>
2024-05-28 15:11:38 +0400

    create init with main v1 api routers

---
+++ containers/vixenhealth_id/app/api/v1/__init__.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+from api.v1.account.account import account_router
+from api.v1.authentication.authentication import token_router
+
+profile_router = APIRouter(prefix="/profile", tags=["profile"])
+
+settings_router = APIRouter(prefix="/settings", tags=["settings"])
+
+account_router.include_router(token_router)
